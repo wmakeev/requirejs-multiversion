55009838d769eb04f0b9522761f99612f3ba1bb8
Vitaliy Makeev <w.makeev@gmail.com>
2015-06-27 17:09:05 +0500

    code reformat, add readme

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 requirejs-multiversion
 ======================
+
+##example
+
+1. Publish dependencies repository to any public url
+
+	```js
+	define({
+        "dependencies": {
+            "lodash": {
+                "2.4.1": "//cdn.jsdelivr.net/lodash/2.4.1/lodash.min.js",
+                "3.0.8": "//cdn.jsdelivr.net/lodash/3.8.0/lodash.min.js"
+            },
+            "moment": {
+                "2.10.3": "//cdn.jsdelivr.net/momentjs/2.10.3/moment.min.js"
+            }
+        }
+    });
+	```
+
+2. Init **requirejs-multiversion** plugin with repository url
+
+	```js
+	requirejs.config({
+	  paths: {
+	    multiver: 'https://path.to/multiver.js'
+	  },
+	  config: {
+	    multiver: {
+	      repositoryUrl: 'https://path.to/repository.js'
+	    }
+	  }
+	});
+	```
+
+3. Define module
+
+  ```js
+  define('my-module', ['multiver!lodash@^2.0.0', 'multiver!lodash@~3.0.7', 'multiver!moment@2.10.3'],
+    function (lodash_1, lodash_2, moment) {
+        return [lodash_1.VERSION, lodash_2.VERSION, moment.version]
+  })
+  ```
+
+4. Require module
+
+  ```js
+  requirejs(['my-module'], function(myModule) {
+    console.log(myModule); // → ["2.4.1", "3.0.8", "2.10.3"]
+  }
+  ```
--- index.js
+++ index.js
@@ -4,66 +4,67 @@ var protocol = window.location.protocol;
 
 module.exports = {
 
-    load: function (name, parentRequire, onload, config) {
-        var multiverConfig  = config.config.multiver;
+  load: function (name, parentRequire, onload, config) {
+    var multiverConfig = config.config.multiver;
 
-        var dependencies    = multiverConfig.dependencies;
-        var resolver        = multiverConfig.resolver;
-        var isFallBack      = !!multiverConfig.fallBackToParentRequire;
+    var dependencies = multiverConfig.dependencies;
+    var resolver = multiverConfig.resolver;
+    var isFallBack = !!multiverConfig.fallBackToParentRequire;
 
-        if (name.indexOf('@') === -1) {
-            name = name + '@default';
-        }
-
-        // need to resolve the dependency
-        var nameVer = name.split('@');
-
-        // i.e. module@^1.0.0
-        if (nameVer.length !== 2) {
-            return onload.error(new Error('Module query must contain only one "@" symbol'));
-        }
-
-        var moduleName    = nameVer[0],
-            versionRange  = nameVer[1];
-
-        if (dependencies[moduleName]) {
-            var versions = Object.keys(dependencies[moduleName]);
-            if (!versions.length) {
-                return onload.error(new Error('No versions specified for module [' + moduleName + ']'))
-            }
-
-            var version;
-            if (versionRange === 'default') {
-                // TODO already loaded or max version
-                version = versions.sort()[versions.length - 1]
-            }
-            else if (!semver.validRange(versionRange)) {
-                return onload.error(new Error('Invalid semver version range [' + versionRange + ']'))
-            }
-            else {
-                version = semver.maxSatisfying(versions, versionRange);
-            }
-
-            if (version) {
-                var path = dependencies[moduleName][version];
-                parentRequire([protocol + path], onload, onload.error);
-            }
-            else if (resolver) {
-                resolver.resolve(moduleName, version, function (err, url) {
-                    if (err) return onload.error(err);
-                    parentRequire([url], onload, onload.error);
-                })
-            }
-            else if (isFallBack) {
-                parentRequire([moduleName], onload, onload.error);
-            }
-            else {
-                onload.error(new Error('Can\'t resolve module [' + moduleName + ']'));
-            }
-        }
-        else {
-            // TODO find module
-            onload.error(new Error('Can\'t find applicable module for [' + name + '] in repository'));
-        }
+    if (name.indexOf('@') === -1) {
+      name = name + '@default';
     }
+
+    // need to resolve the dependency
+    var nameVer = name.split('@');
+
+    // i.e. module@^1.0.0
+    if (nameVer.length !== 2) {
+      return onload.error(new Error('Module query must contain only one "@" symbol'));
+    }
+
+    var moduleName = nameVer[0],
+        versionRange = nameVer[1];
+
+    if (dependencies[moduleName]) {
+      var versions = Object.keys(dependencies[moduleName]);
+      if (!versions.length) {
+        return onload.error(new Error('No versions specified for module [' + moduleName + ']'))
+      }
+
+      var version;
+      if (versionRange === 'default') {
+        // TODO already loaded or max version
+        version = versions.sort()[versions.length - 1]
+      }
+      else if (!semver.validRange(versionRange)) {
+        return onload.error(new Error('Invalid semver version range [' + versionRange + ']'))
+      }
+      else {
+        version = semver.maxSatisfying(versions, versionRange);
+      }
+
+      if (version) {
+        var path = dependencies[moduleName][version];
+        parentRequire([protocol + path], onload, onload.error);
+      }
+      else if (resolver) {
+        resolver.resolve(moduleName, version, function (err, url) {
+          if (err) return onload.error(err);
+          parentRequire([url], onload, onload.error);
+        })
+      }
+      else if (isFallBack) {
+        parentRequire([moduleName], onload, onload.error);
+      }
+      else {
+        onload.error(new Error('Can\'t resolve module [' + moduleName + ']'));
+      }
+    }
+    else {
+      // TODO find module
+      onload.error(new Error('Can\'t find applicable module for [' + name + '] in repository'));
+    }
+  }
+
 };
